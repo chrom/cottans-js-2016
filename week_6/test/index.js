82d4cc383992ad00131bb33d00560933270a2002
Aandrey Zabara <dr.chrom@gmail.com>
2016-06-30 20:24:29 +0300

    hw-lec6

--- week_6/test/index.js
+++ week_6/test/index.js
@@ -1,14 +1,31 @@
-
-
 module.exports = function add(numbersString) {
-    let sum = 0;
+    let sum = 0, delimetr = '', text = '', negative = 0, main = [];
     if (numbersString === '') return 0;
 
-    return numbersString.split(/[\n,]/gi).reduce((previousValue, currentValue) => {
-            if (currentValue !== '')
+    delimetr = numbersString.split(/^[\n\/\/\[]+(\S{1,3})[\]]/ig);
+    if (delimetr.length > 0 && delimetr[1] === undefined) {
+        delimetr = '\n,';
+        text = numbersString;
+    } else {
+        text = delimetr[2];
+        delimetr = delimetr[1];
+    }
+    var reg = new RegExp('[' + delimetr + ']', 'gi');
+    main = text.split(reg);
+    main.reduce((a, b) => {
+        if (b < 0)
+            negative.push(b);
+    });
+    if (negative.length > 0)
+        throw 'negatives not allowed: ' + negative.join(',');
+
+    return main.reduce((previousValue, currentValue) => {
+            if (currentValue > 1000)
+                return previousValue;
+            else if (currentValue !== '')
                 return previousValue + parseInt(currentValue);
             else
                 return previousValue;
         }
-    , 0);
+        , 0);
 }
--- week_6/test/test.js
+++ week_6/test/test.js
@@ -29,7 +29,25 @@ test('Allow the Add method to handle new lines between numbers (instead of comma
 test('Support different delimiters to change a delimiter, ' +
     'the beginning of the string will contain a separate line that looks like this:   ' +
     '“//[delimiter]\n[numbers…]” for example “//;\n1;2” should return three where the default delimiter is ‘;’ .', t => {
-    t.is(add('//;\n1;2'), 1);
+    t.is(add('//;\n1;2'), 3);
+});
+
+test('4.2 the first line is optional. all existing scenarios should still be supported', t => {
+    t.is(add('\n//;\n1;2'), 3);
+});
+
+test('Calling Add with a negative number will throw an exception “negatives not allowed” - ' +
+    'and the negative that was passed. if there are multiple negatives, show all of them in the exception message', t => {
+    t.throws(function(){add('//;-1;-2')}, Error, 'negatives not allowed');
+});
+
+test('Numbers bigger than 1000 should be ignored, so adding 2 + 1001  = 2', t => {
+    t.is(add('//;1;2;1002'), 3);
+});
+
+test('Delimiters can be of any length with the following format:' +
+    '  “//[delimiter]\n” for example: “//[***]\n1***2***3” should return 6', t => {
+    t.is(add('//[***]\n1***2***3'), 6);
 });
 
 
